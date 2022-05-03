6c60bc6a0068b51381f2e21e2e3e22f63434c179
ialnezami <ibrahim.alnezami@gmail.com>
2022-05-03 20:46:00 +0200

    add authorize service :
    this service allow client to get token athorization

---
+++ express_vuejs/server/routes/authorization.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+      'Authorization': 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res, next) {
+  request.post(authOptions, function(error, response, body) {
+          res.status(200).json({body: body});
+     
+  }
+);
+});
+  
+
+module.exports = router;
