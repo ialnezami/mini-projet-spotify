adf5311d850960da80bb3a4473d32111dfa897ab
ialnezami <ibrahim.alnezami@gmail.com>
2022-05-04 23:50:44 +0200

    🔨 script bash
    install and start server and client

---
+++ express_vuejs/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# check if all dependencies are installed for server
+# if not, install them
+if [ ! -f "./server/node_modules/package.json" ]; then
+  echo "Installing express..."
+  cd server
+  npm install
+fi
+# check if all dependencies are installed for client
+# if not, install them
+if [ ! -f "./client/node_modules/package.json" ]; then
+  echo "Installing vue..."
+  cd ../client
+  npm install
+fi
+# start server
+# run the expressjs node server
+cd server
+npm start
+
+# start client
+# run the vuejs app client
+cd ../client
+npm run serve
+# end of file
