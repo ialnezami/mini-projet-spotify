8dcfdaf59f36500c200e6a1035d6d590e32408b3
ialnezami <ibrahim.alnezami@gmail.com>
2022-05-04 07:52:54 +0200

    📦️ add vuex persist

--- express_vuejs/client/src/store/index.js
+++ express_vuejs/client/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex";
 import artisteStore from "@/store/ArtistsStore";
-
+import VuexPersistence from "vuex-persist";
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 export default createStore({
   modules: {
     artisteStore,
   },
+  plugins: [vuexLocal.plugin],
 });
