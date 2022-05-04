8ad0dd15b78583c752c21e7fae3951d2686fee7b
ialnezami <ibrahim.alnezami@gmail.com>
2022-05-04 19:03:03 +0200

    ✨ artist details
    ✨  compenent artist detaills for showing the artist and and his albums

---
+++ express_vuejs/client/src/components/Albums/AlbumView.vue
@@ -0,0 +1,33 @@
+<template>
+  <router-link
+    :to="{
+      path: id + '/album/' + album.id,
+      name: 'albumDetail',
+      params: { id: album.id, idArtist: id },
+    }"
+  >
+    <div class="w-40 h-30 m-3">
+      <div class="">
+        <div class="">
+          <img :src="album.images[0].url" alt="album" class="w-full" />
+        </div>
+        <div class="bg-black h-full">
+          <div class="flex flex-col h-15">
+            <h1 class="font-bold text-xl mb-2 text-white">{{ album.name }}</h1>
+            <p class="text-white">{{ album.release_date }}</p>
+            <p class="text-white">{{ album.total_tracks }} Songs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+<script>
+export default {
+  name: "AlbumView",
+  props: {
+    album: Object,
+    id: String,
+  },
+};
+</script>
---
+++ express_vuejs/client/src/components/Artists/ArtistDetailView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="about">
+    <div class="rounded overflow-hidden shadow-lg w-full">
+      <div class="flex row justify-center">
+        <img
+          :src="
+            artist.images.length > 0
+              ? artist.images[0].url
+              : 'https://thispersondoesnotexist.com/image'
+          "
+          alt="Sunset in the mountains"
+          style="height: 200px"
+          class="rounded-full drop-shadow-2xl"
+        />
+      </div>
+      <div class="absolut shadow top-10 w-full">
+        <div class="px-6 py-4">
+          <h1 class="font-bold text-4xl mb-2 text-white">{{ artist.name }}</h1>
+          <span
+            class="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >{{ artist.followers.total }} Followers
+          </span>
+          <span
+            class="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >{{ artist.popularity }} Followers
+          </span>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span
+            v-for="genre in artist.genres"
+            :key="genre"
+            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >{{ genre }}
+          </span>
+        </div>
+        <div>
+          <div class="px-6 pt-4 pb-2 flex flex-row flex-wrap">
+            <AlbumView
+              v-for="album in albums"
+              :key="album.id"
+              :album="album"
+              :id="artist.id"
+            >
+            </AlbumView>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+import AlbumView from "@/components/Albums/AlbumView.vue";
+
+export default {
+  name: "ArtistDetailView",
+  components: {
+    AlbumView,
+  },
+  computed: {
+    ...mapGetters({
+      getArtists: "getArtists",
+      getArtistAlbums: "getArtistAlbums",
+    }),
+    artist() {
+      return this.getArtists.find(
+        (artist) => artist.id === this.$route.params.id
+      );
+    },
+    albums() {
+      return this.getArtistAlbums;
+    },
+  },
+  mounted() {
+    this.addArtistConsultedAction();
+    this.getArtistAlbumsAction();
+  },
+  methods: {
+    ...mapActions({
+      addArtistConsulted: "addArtistConsulted",
+      getArtistAlbumsStore: "getArtistAlbums",
+    }),
+    async addArtistConsultedAction() {
+      await this.addArtistConsulted(this.$route.params.id);
+    },
+    async getArtistAlbumsAction() {
+      await this.getArtistAlbumsStore(this.$route.params.id);
+    },
+  },
+};
+</script>
